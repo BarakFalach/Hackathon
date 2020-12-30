e31213a784ddcdef44acd99a7398d1fb3b94d85f
BarakFalach <barak8995@gmail.com>
2020-12-30 05:11:20 -0800

    clietn server TCP

--- client.py
+++ client.py
@@ -5,50 +5,33 @@ import sys
 
 def establish_TCP():
 
-    # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Connect the socket to the port where the server is listening
-    server_address = ("192.168.0.68", 5000)
-    print ('connecting to %s port %s' % server_address)
-    sock.connect(server_address)
+    HOST = 'localhost'  # The server's hostname or IP address
+    PORT = 65432        # The port used by the server
 
-    try:
-        
-        # Send data
-        message = 'This is the message.  It will be repeated.'
-        print ('sending "%s"' % message)
-        sock.sendall(message)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(b'Hello, world')
+        data = s.recv(1024)
 
-        # Look for the response
-        amount_received = 0
-        amount_expected = len(message)
-        
-        while amount_received < amount_expected:
-            data = sock.recv(16)
-            amount_received += len(data)
-            print ('received "%s"' % data)
-
-    finally:
-        print ('closing socket')
-        sock.close()
+    print('Received', repr(data))
 
 
 
 
 
-print("Client started, listening for offer requests...")
-UdpClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-UdpClient.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-UdpClient.bind(('', 3000))
+# print("Client started, listening for offer requests...")
+# UdpClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# UdpClient.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+# UdpClient.bind(('', 3000))
 
- # wrong: mreq = struct.pack("sl", socket.inet_aton("224.51.105.104"), socket.INADDR_ANY)
+#  # wrong: mreq = struct.pack("sl", socket.inet_aton("224.51.105.104"), socket.INADDR_ANY)
 
-msg_from_server , b = UdpClient.recvfrom(28)
-msg = struct.unpack('Ibh', msg_from_server)
-print (str(msg))
-print (b)
-print("Received offer from "+b[0] +" attempting to connect...")
+# msg_from_server , b = UdpClient.recvfrom(28)
+# msg = struct.unpack('Ibh', msg_from_server)
+# print (str(msg))
+# print (b)
+# print("Received offer from "+b[0] +" attempting to connect...")
 establish_TCP()
 
 
--- server.py
+++ server.py
@@ -20,19 +20,21 @@ def boradcast():
         count_down-=1
         
 def tcp_connection():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('localhost', 5000)
-    sock.bind(server_address)
-    print ("Before Bind")
-    sock.listen(1)
-    while True:
-    # Wait for a connection
-        print('waiting for a connection')
-        connection, client_address = sock.accept()
-        data = connection.recv(16)
-        if data:
-            print("Got Here")
-            connection.sendall(data)
+
+    HOST = 'localhost'  # Standard loopback interface address (localhost)
+    PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
 
         
 

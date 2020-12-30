d9fe460310b7787ebb673eaee278a5bcff75fd34
BarakFalach <barak8995@gmail.com>
2020-12-30 03:23:33 -0800

    3rd commit

--- client.py
+++ client.py
@@ -2,14 +2,13 @@
 import socket
 import struct
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+UdpClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+UdpClient.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+UdpClient.bind(('', 3000))
 
-sock.bind(('', 3000))
  # wrong: mreq = struct.pack("sl", socket.inet_aton("224.51.105.104"), socket.INADDR_ANY)
-mreq = struct.pack("=4sl", socket.inet_aton("192.168.0.68"), socket.INADDR_ANY)
-
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 while True:
-    print(sock.recv(10240))
+    msg_from_server , b = UdpClient.recvfrom(28)
+    msg = struct.unpack('Ibh', msg_from_server)
+    print (str(msg))
--- server.py
+++ server.py
@@ -1,15 +1,19 @@
 
 import socket
 import time
+import struct
 
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
 # Set a timeout so the socket does not block
 # indefinitely when trying to receive data.
 server.settimeout(0.2)
-server.bind(("192.168.0.68", 3000))
-message = b"your very important message"
+# server.bind(("localhost", 3000))
+message = struct.pack("Ibh",1000,0x2,5000)
 while True:
-    server.sendto(message, ('<broadcast>', 37020))
+    server.sendto(message, ('<broadcast>', 3000))
     print("message sent!")
     time.sleep(1)

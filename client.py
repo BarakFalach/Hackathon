7e8f5d25eb00eb9643977921ec8a8fa509a381e7
amit-os <amitos@post.bgu.ac.il>
2020-12-30 23:35:01 +0200

    clientim

--- client.py
+++ client.py
@@ -9,15 +9,14 @@ import time
 def start_clicking(s):
     # readable, writable, exceptional = select.select(inputs, outputs, inputs)
     print("start clicking!!!")
-    dead_line = time.time() + 5
-    t = 0 
-    while t < dead_line:
-        pressedKey = msvcrt.getch()
-        s.sendall(pressedKey)
-        t = time.time()
+    dead_line = time.time() + 10
+    while time.time() < dead_line:
+         if msvcrt.kbhit():
+            pressedKey = msvcrt.getch()
+            s.sendall(pressedKey)
     print('stop clicking!')
-    data = s.recv(1024).decode('utf-8')
-    print(repr(data))    
+    the_winner_is = s.recv(1024).decode()
+    print(the_winner_is)    
 
 def establish_TCP(host_ip, port_to_connect,client_name):
 
@@ -29,9 +28,9 @@ def establish_TCP(host_ip, port_to_connect,client_name):
         print("before connection")
         s.connect((HOST, PORT))
         print("after connection before sending")
-        s.sendall(bytes(client_name+"\n", encoding='utf-8'))
-        data = s.recv(1024).decode('utf-8')
-        print('Received', repr(data))
+        s.sendall((client_name +"\n").encode())
+        data = s.recv(1024).decode()
+        print(data)
         start_clicking(s)
         
     
@@ -42,21 +41,21 @@ def establish_TCP(host_ip, port_to_connect,client_name):
 
 
 client_name = "cheesy_crust"
-# while True:
-print("Client started, listening for offer requests...")
-UdpClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-UdpClient.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-UdpClient.bind(('', 3000))
+while True:
+    print("Client started, listening for offer requests...")
+    UdpClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    UdpClient.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    UdpClient.bind(('', 3000))
 
-# wrong: mreq = struct.pack("sl", socket.inet_aton("224.51.105.104"), socket.INADDR_ANY)
+    # wrong: mreq = struct.pack("sl", socket.inet_aton("224.51.105.104"), socket.INADDR_ANY)
 
-msg_from_server , b = UdpClient.recvfrom(28)
-msg = struct.unpack('Ibh', msg_from_server)
-print (str(msg))
-print (b)
-print("Received offer from "+b[0] +" attempting to connect...")
+    msg_from_server , b = UdpClient.recvfrom(28)
+    msg = struct.unpack('Ibh', msg_from_server)
+    print (str(msg))
+    print (b)
+    print("Received offer from "+b[0] +" attempting to connect...")
 
-establish_TCP(b[0],msg[2],client_name)
+    establish_TCP(b[0],msg[2],client_name)
 
 
 
